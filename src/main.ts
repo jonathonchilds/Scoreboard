5929ca1335f85553d07f959e21d9a36fd98ea52b
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-10-17 13:49:32 -0600

    ability to change team name, but only changes h2 for team 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-import "./style.css";
+import './style.css'
+const inputElements = document.querySelectorAll('input')
+const paragraphElement: any = document.querySelector('h2')
+function updateParagraph(event: Event) {
+  const elementChanged = event.target
+  if (elementChanged instanceof HTMLInputElement) {
+    const currentInputValue = elementChanged.value
+    paragraphElement.innerText = currentInputValue
+  }
+}
+// Attach event listeners to each one
+inputElements.forEach((element) =>
+  element.addEventListener('input', updateParagraph)
+)
