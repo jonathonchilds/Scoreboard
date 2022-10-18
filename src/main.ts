735363f8c1aa67f5ddc9b9b1d4ee9ec6c8c4d431
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-10-18 13:25:06 -0600

    separate querySelectors and EventListeners for each team - working for Player 1 only

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,36 @@
 import './style.css'
 //                                  ||||||
 // reads from section.team only    \/\/\/
-const inputElements = document.querySelectorAll('input') 
+const team1Input = document.querySelector('input' && 'section.team1') 
+console.log(team1Input)
 
-//                             ||||||
-// updates section.team1, h2    \/\/\/
-const paragraphElement: any = document.querySelector('section.team1' && 'h2')
+const team2input = document.querySelector('input' && 'section.team2')
+console.log(team2input)
 
+const team2Name: any = document.querySelector('section.team2' && 'h2')
+console.log(team2Name)
 
 function updateParagraph(event: Event) {
   const elementChanged = event.target
-  if (elementChanged instanceof HTMLInputElement) {
+  if (elementChanged instanceof HTMLInputElement) 
+  {
     const currentInputValue = elementChanged.value
-    paragraphElement.innerText = currentInputValue
+    team2Name.innerText = currentInputValue
+    // if (elementChanged instanceof team1Input)
+    // {
+    // const currentInputValue = elementChanged.value
+    // team1Name.innerText = currentInputValue
+    // }
+    // else if (elementChanged instanceof team2input)
+    // {
+    //   const currentInputValue = elementChanged.value
+    // team2Name.innerText = currentInputValue
+    // }
   }
 }
 
-inputElements.forEach((element) => element.addEventListener('input', updateParagraph))
+
+team1Input?.addEventListener('input', updateParagraph)
+
+team2input?.addEventListener('input', updateParagraph)
+
