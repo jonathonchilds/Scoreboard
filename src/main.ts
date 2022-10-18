4f2bb61e4d5598df47ac6f06f5167fabdc877d6f
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-10-17 21:43:49 -0600

    team 1 name change working

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './style.css'
-const inputElements = document.querySelectorAll('input')
-const paragraphElement: any = document.querySelector('h2')
+//                                  ||||||
+// reads from section.team only    \/\/\/
+const inputElements = document.querySelectorAll('input' && 'section.team1') 
+
+//                             ||||||
+// updates section.team1, h2    \/\/\/
+const paragraphElement: any = document.querySelector('section.team1' && 'h2')
+
+
 function updateParagraph(event: Event) {
   const elementChanged = event.target
   if (elementChanged instanceof HTMLInputElement) {
@@ -8,7 +15,5 @@ function updateParagraph(event: Event) {
     paragraphElement.innerText = currentInputValue
   }
 }
-// Attach event listeners to each one
-inputElements.forEach((element) =>
-  element.addEventListener('input', updateParagraph)
-)
+
+inputElements.forEach((element) => element.addEventListener('input', updateParagraph))
