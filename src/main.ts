d3829a9be53c03aff5c6a505a5ec6e0061fde932
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-10-18 17:13:41 -0600

    adds Reset button

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,8 @@ function teamContent(id: string)
   const teamNameInputBox = document.querySelector(`.${id} input`) as HTMLInputElement
   const incrementScoreButton = document.querySelector(`.${id} .add`) as HTMLElement
   const decreaseScoreButton = document.querySelector(`.${id} .subtract`) as HTMLElement
-
+  const resetScoreButton = document.querySelector(`.${id} .reset`) as HTMLElement
+  
   // OUTPUTS  |
   //          |
   //          V
@@ -20,36 +21,50 @@ function teamContent(id: string)
 
 
   function updateTeamName(event: Event) 
-  {
-    teamNameDisplay.innerText = (event.target as HTMLInputElement).value
-  };
-  teamNameInputBox.addEventListener('input', updateTeamName)
+    {
+      teamNameDisplay.innerText = (event.target as HTMLInputElement).value
+    }
+  
+    teamNameInputBox.addEventListener('input', updateTeamName)
 
 
   function incrementScore()
-  {
-    if (teamScoreCounter < 21)
     {
-      teamScoreCounter = teamScoreCounter + 1
-      teamScoreDisplay.textContent = `${teamScoreCounter}`
+      if (teamScoreCounter < 21)
+      {
+        teamScoreCounter = teamScoreCounter + 1
+        teamScoreDisplay.textContent = `${teamScoreCounter}`
+      }
+      else teamScoreCounter = teamScoreCounter
     }
-    else teamScoreCounter = teamScoreCounter
-  }
-  incrementScoreButton.addEventListener('click', incrementScore)
+  
+    incrementScoreButton.addEventListener('click', incrementScore)
 
 
   function decreaseScore()
-  {
-    if (teamScoreCounter > 0)
     {
-    teamScoreCounter = teamScoreCounter - 1
-    teamScoreDisplay.textContent = `${teamScoreCounter}`
+      if (teamScoreCounter > 0)
+      {
+      teamScoreCounter = teamScoreCounter - 1
+      teamScoreDisplay.textContent = `${teamScoreCounter}`
+      }
+      else teamScoreCounter = teamScoreCounter
     }
-    else teamScoreCounter = teamScoreCounter
-  }
-  decreaseScoreButton.addEventListener('click', decreaseScore)
+  
+    decreaseScoreButton.addEventListener('click', decreaseScore)
+
+
+  function resetScore()
+    {
+      teamScoreCounter = 0
+      teamScoreDisplay.textContent = `${teamScoreCounter}`
+    }
+  
+    resetScoreButton.addEventListener('click', resetScore)
+
 
 }
+
 teamContent('team1')
 teamContent('team2')
 
