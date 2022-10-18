ed9b7e983e56967c7e214edd27ef215c1be32916
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-10-18 15:43:03 -0600

    adds ability to decrease score

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,22 @@ import './style.css'
 
 function teamContent(id: string)
 {
+
   let teamScoreCounter = 0;
+
   // INPUTS  |
   //         |
   //         V
-  const teamNameInputBox = document.querySelector(`.${id} input`) as HTMLInputElement;
+  const teamNameInputBox = document.querySelector(`.${id} input`) as HTMLInputElement
   const incrementScoreButton = document.querySelector(`.${id} .add`) as HTMLElement
-  
+  const decreaseScoreButton = document.querySelector(`.${id} .subtract`) as HTMLElement
+
   // OUTPUTS  |
   //          |
   //          V
-  const teamNameDisplay = document.querySelector(`.${id} h2`) as HTMLInputElement;
+  const teamNameDisplay = document.querySelector(`.${id} h2`) as HTMLInputElement
   const teamScoreDisplay = document.querySelector(`.${id} h3`) as HTMLHeadingElement
+
 
   function updateTeamName(event: Event) 
   {
@@ -21,12 +25,21 @@ function teamContent(id: string)
   };
   teamNameInputBox.addEventListener('input', updateTeamName)
 
+
   function incrementScore()
   {
-    teamScoreCounter = teamScoreCounter + 1;
+    teamScoreCounter = teamScoreCounter + 1
     teamScoreDisplay.textContent = `${teamScoreCounter}`
   }
   incrementScoreButton.addEventListener('click', incrementScore)
+
+
+  function decreaseScore()
+  {
+    teamScoreCounter = teamScoreCounter - 1
+    teamScoreDisplay.textContent = `${teamScoreCounter}`
+  }
+  decreaseScoreButton.addEventListener('click', decreaseScore)
 
 }
 teamContent('team1')
